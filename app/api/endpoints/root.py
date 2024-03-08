99b6fd20972255b995d5c7b43ffc0d2a33b0f70e
Alex Castillo <alex@lexdrel.com>
2024-03-08 10:10:26 -0600

    save mock files in assets folder inside the instance

--- app/api/endpoints/root.py
+++ app/api/endpoints/root.py
@@ -1,5 +1,8 @@
 """Defines the router for the root path (/)."""
 from fastapi import APIRouter
+import os
+import uuid
+from datetime import datetime
 
 
 ROUTER = APIRouter()
@@ -8,6 +11,13 @@ ROUTER = APIRouter()
 @ROUTER.get("/")
 async def root():
     """Handles the root endpoint, providing information about the API."""
+
+    if not os.path.exists("assets"):
+        os.makedirs("assets")
+
+    file_name = f"assets/current{uuid.uuid4()}.py"
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(f"# Created at: {datetime.now()}\n")
 
     version = "0.0.0"
     return {
